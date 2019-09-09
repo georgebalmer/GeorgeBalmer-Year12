ca37af0d1fb726ab619877b4e8952ee57ef13196
George Balmer <007george.b@gmail.com>
2019-09-09 15:46:45 +0100

    fixing css for shackleton

---
+++ Acs Prog Tasks/comparison_of_three.py
@@ -0,0 +1,13 @@
+i_num1 = int(input("please enter a number"))
+i_num2 = int(input("please enter another number"))
+i_num3 = int(input("please enter another nnumber"))
+
+if i_num1 > i_num2 and i_num1 > i_num3:
+    print(i_num1 and i_num2 and i_num3)
+
+if i_num2 > i_num3 and i_num2 > i_num1:
+    print(i_num2 and i_num1 and i_num3)
+
+if i_num3 > i_num2 and i_num3 > i_num1:
+    print(i_num3 and i_num1 and i_num2)
+
---
+++ Acs Prog Tasks/comparison_of_two.py
@@ -0,0 +1,10 @@
+i_num1 = int(input("please enter a number"))
+i_num2 = int(input("please enter another number"))
+if i_num1 > i_num2:
+    print(i_num2 and i_num1)
+
+else:
+    print(i_num1 and i_num2)
+
+#end if
+
